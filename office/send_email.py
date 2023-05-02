55dedc9753cb85f172c07a7b41410646ff7627b3
alexwang <alexwl2008@gmail.com>
2023-05-02 14:22:48 +0800

    ws#1 update email for smtp

--- office/send_email.py
+++ office/send_email.py
@@ -1,57 +1,59 @@
+import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
+import os
 
-import openpyxl
 
+def send_email(filepath):
+    # 读取 Excel 文件
+    df = pd.read_excel(filepath, sheet_name="email", header=0)
 
-def send_email(excel_path):
-    # 打开Excel文件
-    wb = openpyxl.load_workbook(excel_path)
-    ws = wb.active
+    # 设置邮件信息
+    print(df.iloc[0])
+    print(df.iloc[0]['Name'])
+    subject = "主题图片"
+    body = "{0}您好，您要的图片在附件里".format(df.iloc[0]['Name'])
+    from_addr = "69692418@qq.com"
+    password = "rock200*"
+    smtp_server = "smtp.qq.com"
+    smtp_port = 587
+    to_addr = df.iloc[0]['Email']
+    bless = df.iloc[0]['End']
+    image_path = df.iloc[0]['Pic']
 
-    # 获取电子邮件地址、主题、邮件主体、附件路径、发送人和祝福语
-    sTo = ws.cell(row=2, column=1).value
-    sSubject = "主题图片"
-    sX = ws.cell(row=2, column=2).value
-    sBody = sX + "您好，您要的图片在附件里"
-    sGreeting = ws.cell(row=2, column=4).value
-    sPath = ws.cell(row=2, column=5).value
-
-    # 创建电子邮件对象
+    # 创建邮件对象
     msg = MIMEMultipart()
-    msg['To'] = sTo
-    msg['Subject'] = sSubject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = subject
 
-    # 添加文本内容
-    body = sBody + '\n\n' + sGreeting
+    # 添加邮件正文
     msg.attach(MIMEText(body, 'plain'))
 
     # 添加图片附件
-    with open(sPath, 'rb') as f:
-        img_data = f.read()
-    image = MIMEImage(img_data, name=sPath)
-    msg.attach(image)
+    if os.path.isfile(image_path):
+        with open(image_path, 'rb') as f:
+            img_data = f.read()
+        image = MIMEImage(img_data, name=os.path.basename(image_path))
+        msg.attach(image)
 
-    # 发送电子邮件
+    # 添加祝福语
+    msg.attach(MIMEText(bless, 'plain'))
+
+    # 发送邮件
     try:
-        server = smtplib.SMTP('localhost')
-        server.sendmail('', sTo, msg.as_string())
-        print("电子邮件发送成功")
-    except:
-        print("Error: 无法发送电子邮件")
-
-    # 关闭SMTP对象
-    server.quit()
-
-
-def send_email_prompt():
-    # 提示用户输入Excel文件路径
-    excel_path = input("请输入Excel文件路径: ")
-
-    # 调用send_email()函数发送电子邮件
-    send_email(excel_path)
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(from_addr, password)
+        server.sendmail(from_addr, to_addr, msg.as_string())
+        server.quit()
+        print("邮件发送成功！")
+    except Exception as e:
+        print("邮件发送失败：", e)
 
 
-send_email_prompt()
+send_email("/Users/alexwang/Projects/pythonUtil/office/formula.xlsx")
+
+# send_email("/Users/alexwang/Projects/pythonUtil/office/formula.xlsx", "smtp.qq.com", 465, "69692418@qq.com", "rock200*")
